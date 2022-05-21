ef7b7893d5bb31833921b3452b2beec26ef0b4e7
Mugundhan Govindaraju <mugu555ngr@gmail.com>
2022-05-21 14:20:21 -0400

    My first commit

---
+++ src/main/java/com/example/demo/SpringBootTestForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestForGitApplication.class, args);
+	}
+
+}
